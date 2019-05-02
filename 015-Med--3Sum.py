b1d8e431421c951557abfcdac80dab146eeee862
Mario <beaulieu.mario@gmail.com>
2019-05-02 19:53:04 -0400

    Success on the 3Sum.py program

--- 015-Med--3Sum.py
+++ 015-Med--3Sum.py
@@ -6,6 +6,7 @@ from typing import List
 class Solution:
     def binSearch(self, v:int, li:List[int]) -> int:
         lll = ll = len(li)
+        if ll<2: return 0
         i  = ll//2
         while ll>1:
             ll = (ll+1)//2
@@ -16,7 +17,9 @@ class Solution:
                 i += ll
                 if i>=lll: i=lll-1
             else:
-                return i
+                break 
+        while i>0 and li[i-1]==li[i]:
+            i-=1
         return i
     def threeSum(self,nums: List[int]) -> List[List[int]]:
         res = []
@@ -31,21 +34,40 @@ class Solution:
             vi=nums[i]
             if vi != pvi:
                 pvi= vi
-                pvj= mi-1
                 # Minimum value for vj will be -vi-ma
                 minj = self.binSearch(-vi-ma, nums[i+1:-1])+i+1
                 #print('searching value %i in list yields minj=%i, list='%(-vi-ma,minj-i-1),nums[i+1:-1])
-                for j in range(minj,ln-1):
-                    vj=nums[j]
-                    if vj != pvj:
-                        pvj= vj
-                        v  =-vi-vj
-                        if mi <= v <= ma :
-                            if v in nums[j+1:]:
-                                #r = [vi,vj,v]
-                                #r.sort()
-                                #if r not in res:
-                                res.append([vi,vj,v])
+                j=minj
+                vj=nums[j]
+                k=ln-1
+                vk=nums[k]
+                while j<k:
+                    v=vi+vj+vk
+                    #print('(i,j,k)=(%i,%i,%i) -> (vi,vj,vk)=(%i,%i,%i) -> Sum = %i'%(i,j,k,vi,vj,vk,v))
+                    if v<0:
+                        pvj=vj
+                        while pvj==vj:
+                            j+=1
+                            if j>=ln: break
+                            vj=nums[j]
+                    elif v>0:
+                        pvk= vk
+                        while pvk==vk:
+                            k-=1
+                            if k<=1: break
+                            vk=nums[k]
+                    else:
+                        res.append([vi,vj,vk])
+                        pvj=vj
+                        while pvj==vj:
+                            j+=1
+                            if j>=ln: break
+                            vj=nums[j]
+                        pvk= vk
+                        while pvk==vk:
+                            k-=1
+                            if k<=1: break
+                            vk=nums[k]
         return res
 
 if len(sys.argv) < 4:
@@ -53,6 +75,6 @@ if len(sys.argv) < 4:
 else:
     S=Solution()
     v=list(map(int,sys.argv[1:]))
-    #for x in v:
-    #    print(S.binSearch(x,v))
+    #for x in range(v[0],v[-1]):
+    #    print('%i --> %i'%(x,S.binSearch(x,v)))
     print('List of triplets with sum is 0:\n',S.threeSum(v))
